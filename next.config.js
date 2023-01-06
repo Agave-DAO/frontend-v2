e60adf51012af3b86b5447dd88aca4a141c430e8
fernandomg <fernando.greco@gmail.com>
2023-01-06 16:10:48 +0100

    feat: add gnosis tokenlists
    also whitelist images sources

--- next.config.js
+++ next.config.js
@@ -10,7 +10,12 @@ module.exports = withBundleAnalyzer({
     domains: [
       "tokens.1inch.io",
       "ethereum-optimism.github.io",
-      "assets.coingecko.com"
+      "assets.coingecko.com",
+      "raw.githubusercontent.com",
+      "elk.finance",
+      "api.rubic.exchange",
+      "daodkp.oss-ap-southeast-1.aliyuncs.com",
+      "cloudstorage.openocean.finance",
     ]
   }
 });
