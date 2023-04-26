86eff4b721f895e1d6278d73babda8c77fb0369a
Gabito Esmiapodo <gabriel@bootnode.dev>
2023-04-26 15:19:42 -0300

    refactor: create specific components for some parts of the vault section

--- pages/strategies/[strategyName]/index.tsx
+++ pages/strategies/[strategyName]/index.tsx
@@ -3,25 +3,19 @@ import { useRouter } from 'next/router'
 import { useMemo, useState } from 'react'
 import styled from 'styled-components'
 
-import { Asset, Body, Head } from '@/src/components/asset/Asset'
-import { ActionButton } from '@/src/components/buttons/ActionButton'
 import { List } from '@/src/components/common/List'
-import { MoreActionsDropdown } from '@/src/components/dropdown/MoreActionsDropdown'
 import { GoToExplorer } from '@/src/components/helpers/GoToExplorer'
-import { ActionsWrapper } from '@/src/components/layout/ActionsWrapper'
 import { ButtonTab, ButtonTabs } from '@/src/components/tabs/ButtonTabs'
 import { BaseTitle } from '@/src/components/text/BaseTitle'
 import { CollateralSwap, Long, Short } from '@/src/pagePartials/strategy/strategies/StrategyItem'
-import { VaultInfo } from '@/src/pagePartials/strategy/vaults/VaultInfo'
-import { useVaultModalContext } from '@/src/providers/vaultModalProvider'
+import { VaultDetails as BaseVaultDetails } from '@/src/pagePartials/strategy/vaults/VaultDetails'
 
 const Title = styled(BaseTitle)`
   margin: 0 0 40px;
   text-transform: capitalize;
 `
 
-const VaultDetails = styled(Asset)`
-  height: auto;
+const VaultDetails = styled(BaseVaultDetails)`
   margin: 0 0 32px;
 `
 
@@ -36,37 +30,30 @@ const Strategy: NextPage = () => {
   const [positions, setPositions] = useState<Array<number>>([])
   // this is obviously just a placeholder
   const getStrategyAddress = '0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb'
-  const { openCreateVaultModal } = useVaultModalContext()
-  const items = [
-    {
-      text: 'Edit name',
-      onClick: () => openCreateVaultModal(getStrategyAddress),
-    },
-    {
-      text: 'Withdraw',
-      onClick: () => console.log('withdraw'),
-    },
-  ]
   const [activeTab, setActiveTab] = useState<'positions' | 'history' | 'newStrategy'>('positions')
+  const showPositionsList = useMemo(() => activeTab === 'positions', [activeTab])
+  const showHistoryList = useMemo(() => activeTab === 'history', [activeTab])
+  const showNewStrategyList = useMemo(() => activeTab === 'newStrategy', [activeTab])
+
   const tabs = useMemo(
     () => [
       {
         text: 'Positions',
         onClick: () => setActiveTab('positions'),
-        isActive: activeTab === 'positions',
+        isActive: showPositionsList,
       },
       {
         text: 'History',
         onClick: () => setActiveTab('history'),
-        isActive: activeTab === 'history',
+        isActive: showHistoryList,
       },
       {
         text: 'New Strategy',
         onClick: () => setActiveTab('newStrategy'),
-        isActive: activeTab === 'newStrategy',
+        isActive: showNewStrategyList,
       },
     ],
-    [activeTab],
+    [showHistoryList, showNewStrategyList, showPositionsList],
   )
 
   const createPosition = () => {
@@ -90,17 +77,7 @@ const Strategy: NextPage = () => {
         <span>Vault {strategyName}</span>
         <GoToExplorer address={getStrategyAddress} text="Vault" />
       </Title>
-      <VaultDetails>
-        <Head>
-          <VaultInfo />
-        </Head>
-        <Body>
-          <ActionsWrapper>
-            <MoreActionsDropdown items={items} size="lg" />
-            <ActionButton onClick={() => console.log('Deposit')}>Deposit</ActionButton>
-          </ActionsWrapper>
-        </Body>
-      </VaultDetails>
+      <VaultDetails />
       {positions.length ? (
         <>
           <ButtonTabs>
@@ -110,9 +87,9 @@ const Strategy: NextPage = () => {
               </ButtonTab>
             ))}
           </ButtonTabs>
-          {activeTab === 'positions' && <>Positions list</>}
-          {activeTab === 'history' && <>History list</>}
-          {activeTab === 'newStrategy' && <>{strategyCreationItems}</>}
+          {showPositionsList && <>Positions list</>}
+          {showHistoryList && <>History list</>}
+          {showNewStrategyList && <>{strategyCreationItems}</>}
         </>
       ) : (
         <>
