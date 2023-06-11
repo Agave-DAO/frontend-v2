26f976b2819498c50640fe0e186cb9f61f44e475
fernandomg <fernando.greco@gmail.com>
2023-06-11 20:40:17 +0200

    fix: avoid importing the named export 'strategies'
    warn  - ./src/constants/strategiesInfo.tsx
    Should not import the named export 'strategies'.'map' (imported as 'strategies') from default-exporting module (only default export is available soon)

--- src/constants/strategiesInfo.tsx
+++ src/constants/strategiesInfo.tsx
@@ -1,14 +1,16 @@
 import { ReactElement } from 'react'
 
-import { strategies } from './strategies.json'
+import strategiesJSON from './strategies.json'
 import { CollateralSwap } from '@/src/components/assets/CollateralSwap'
 import { Strategy } from '@/types/strategy'
+
+const strategies = strategiesJSON?.strategies
 
 export const strategyIcon: Record<Strategy['slug'], ReactElement> = {
   'collateral-swap': <CollateralSwap />,
 }
 
-export const strategiesInfo: Strategy[] = strategies.map((strategy) => {
+export const strategiesInfo = strategies.map((strategy): Strategy => {
   if (!strategyIcon[strategy.slug]) {
     throw new Error(`Missing icon for ${strategy.slug}`)
   }
